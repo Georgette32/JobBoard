b51ff588ad3cb6d1d886d668f52f83c6cc490acb
Georgette32 <georgette32@gmail.com>
2026-09-23 15:28:01 +0300

    feat: implement close job with CQRS and MediatR

---
+++ JobBoard.Application/Features/Jobs/Commands/CloseJobCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBoard.Application.Features.Jobs.Commands
+{
+    public record CloseJobCommand(int jobId , int recruiterId) :
+        IRequest;
+}
--- JobBoard.Application/Services/JobService.cs
+++ JobBoard.Application/Services/JobService.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using JobBoard.Application.Interfaces;
-using JobBoard.Domain.Enums;
+
 namespace JobBoard.Application.Services
 {
     public class JobService
@@ -14,23 +13,6 @@ namespace JobBoard.Application.Services
         {
             _jobRepository = jobRepository;
         }
-        public async Task CloseJobAsync(int jobId, int recruiterId)
-        {
-            var job = await _jobRepository.GetByIdAsync(jobId);
-            if (job == null)
-            {
-                throw new KeyNotFoundException("job not found");
-            }
-            if (job.RecruiterId != recruiterId)
-            {
-                throw new UnauthorizedAccessException("You are not the owner of this job.");
-            }
-            if (job.Status != JobStatus.Active)
-            {
-                throw new InvalidOperationException("Only active jobs can be closed.");
-            }
-            job.Close(recruiterId);
-            await _jobRepository.SaveChangesAsync();
-        } 
+
     }
 }
